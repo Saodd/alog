91b58303a3823911dde1e32d1322073864e2d06a
Lin Lan <lanlin@meideng.net>
2021-09-10 12:13:27 +0800

    MOD: logger增加打印代码位置

--- logger.go
+++ logger.go
@@ -5,6 +5,6 @@ import (
 	"os"
 )
 
-var RECOVER = log.New(os.Stdout, "[RECOVER]", log.LstdFlags)
-var ERROR = log.New(os.Stdout, "[ERROR]", log.LstdFlags)
-var INFO = log.New(os.Stdout, "[INFO]", log.LstdFlags)
+var RECOVER = log.New(os.Stdout, "[RECOVER]", log.LstdFlags|log.Lshortfile)
+var ERROR = log.New(os.Stdout, "[ERROR]", log.LstdFlags|log.Lshortfile)
+var INFO = log.New(os.Stdout, "[INFO]", log.LstdFlags|log.Lshortfile)
